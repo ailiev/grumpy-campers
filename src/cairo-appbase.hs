08234bc2551f7b9c88613e44ebd0eaf7e35a621e
Alex Iliev <alex.iliev@gmail.com>
2010-08-14 23:13:28 +0100

    Extracted GUI boilerplate into a helper function out of the way.

--- src/cairo-appbase.hs
+++ src/cairo-appbase.hs
@@ -33,76 +33,10 @@ windowWidth, windowHeight :: Int
 windowWidth   = 900
 windowHeight  = 900
 
--- Write image to file
-writePng :: IO ()
-writePng =
-  C.withImageSurface C.FormatARGB32 width height $ \ result -> do
-      C.renderWith result $ example width height []
-      C.surfaceWriteToPNG result "Draw.png"
-  where width  = windowWidth
-        height = windowHeight
-
 -- Display image in window
 main = do
   G.initGUI
-
-  -- load up the glade file
-  filename <- My.getDataFileName "data/main.glade"
-  windowXmlM <- Glade.xmlNew filename
-  let windowXml = case windowXmlM of
-        (Just wX) -> wX
-        Nothing -> error ("can't find the glade file " ++ filename)
-      get :: (G.WidgetClass widget) => (GTypes.GObject -> widget) -> String -> IO widget
-      get = Glade.xmlGetWidget windowXml
-
-  -- get a handle on widgets from the glade file
-  window <- get G.castToWindow "window1"
-
-  -- set up File-> New
-  new1 <- get G.castToMenuItem "new1"
-  G.onActivateLeaf new1 $ myNew
-
-  -- set up the File-> Open dialog
-  open1 <- get G.castToMenuItem "open1"
-  openDialog <- get G.castToFileChooserDialog "opendialog"
-  G.onActivateLeaf open1 $ G.widgetShow openDialog
-  G.onResponse openDialog $ myFileOpen openDialog
-
-  -- set up the File-> Save_As dialog
-  save1 <- get G.castToMenuItem "save1"
-  save_as1 <- get G.castToMenuItem "save_as1"
-  saveDialog <- get G.castToFileChooserDialog "savedialog"
-  G.onActivateLeaf save_as1 $ G.widgetShow saveDialog
-  G.onActivateLeaf save1 $ G.widgetShow saveDialog
-  G.onResponse saveDialog $ myFileSave saveDialog
-
-  -- set up Edit menu
-  cut1 <- get G.castToMenuItem "cut1"
-  G.onActivateLeaf cut1 $ myCut
-  copy1 <- get G.castToMenuItem "copy1"
-  G.onActivateLeaf copy1 $ myCopy
-  paste1 <- get G.castToMenuItem "paste1"
-  G.onActivateLeaf paste1 $ myPaste
-  delete1 <- get G.castToMenuItem "delete1"
-  G.onActivateLeaf delete1 $ myDelete
-
-  -- set up the Help-> About dialog
-  about1 <- get G.castToMenuItem "about1"
-  aboutdialog1 <- get G.castToAboutDialog "aboutdialog1"
-  G.onActivateLeaf about1 $ G.widgetShow aboutdialog1
-  G.onResponse aboutdialog1 $ const $ G.widgetHide aboutdialog1
-
-  -- fix size
-  --   G.windowSetResizable window False
-  G.widgetSetSizeRequest window windowWidth windowHeight
-
-  -- quit on File-> Quit menu selection
-  quit1 <- get G.castToMenuItem "quit1"
-  G.onActivateLeaf quit1 $ G.widgetDestroy window
-  G.onDestroy window G.mainQuit
-
-  -- set up the canvas
-  canvas <- get G.castToDrawingArea "drawingarea1"
+  (window, canvas) <- setupGUIBoilerplate
 
   -- seems to be enabled by default
   -- G.widgetAddEvents canvas [Widget.ButtonPressMask]
@@ -119,6 +53,8 @@ main = do
   let points = (!! 200) $ campingLocations (cCANVAS_SIDE,cCANVAS_SIDE) (12,7)
   G.onExpose canvas $ const (updateCanvas canvas points)
   G.mainGUI
+
+
 
 -- | Translate canvas coords from window domain to user domain.
 translateCoords :: G.DrawingArea -> Double -> Double -> IO (Int,Int)
@@ -160,20 +96,6 @@ myFileOpen fcdialog response = do
     G.ResponseClose -> putStrLn "Closed!"
   G.widgetHide fcdialog
 
-myFileSave :: G.FileChooserDialog -> G.ResponseId -> IO ()
-myFileSave = myFileOpen
-
-myCut :: IO ()
-myCut = putStrLn "Cut"
-
-myCopy :: IO ()
-myCopy = putStrLn "Copy"
-
-myPaste :: IO ()
-myPaste = putStrLn "Paste"
-
-myDelete :: IO ()
-myDelete = putStrLn "Delete"
 
 updateCanvas :: G.DrawingArea -> [Point] -> IO Bool
 updateCanvas canvas points = do
@@ -287,3 +209,91 @@ grid xmin xmax ymin ymax =
 real = fromIntegral
 
 ----------------------------------------------------------------
+
+
+setupGUIBoilerplate = do
+  -- load up the glade file
+  filename <- My.getDataFileName "data/main.glade"
+  windowXmlM <- Glade.xmlNew filename
+  let windowXml = case windowXmlM of
+        (Just wX) -> wX
+        Nothing -> error ("can't find the glade file " ++ filename)
+      get :: (G.WidgetClass widget) => (GTypes.GObject -> widget) -> String -> IO widget
+      get = Glade.xmlGetWidget windowXml
+
+  -- get a handle on widgets from the glade file
+  window <- get G.castToWindow "window1"
+
+  -- set up File-> New
+  new1 <- get G.castToMenuItem "new1"
+  G.onActivateLeaf new1 $ myNew
+
+  -- set up the File-> Open dialog
+  open1 <- get G.castToMenuItem "open1"
+  openDialog <- get G.castToFileChooserDialog "opendialog"
+  G.onActivateLeaf open1 $ G.widgetShow openDialog
+  G.onResponse openDialog $ myFileOpen openDialog
+
+  -- set up the File-> Save_As dialog
+  save1 <- get G.castToMenuItem "save1"
+  save_as1 <- get G.castToMenuItem "save_as1"
+  saveDialog <- get G.castToFileChooserDialog "savedialog"
+  G.onActivateLeaf save_as1 $ G.widgetShow saveDialog
+  G.onActivateLeaf save1 $ G.widgetShow saveDialog
+  G.onResponse saveDialog $ myFileSave saveDialog
+
+  -- set up Edit menu
+  cut1 <- get G.castToMenuItem "cut1"
+  G.onActivateLeaf cut1 $ myCut
+  copy1 <- get G.castToMenuItem "copy1"
+  G.onActivateLeaf copy1 $ myCopy
+  paste1 <- get G.castToMenuItem "paste1"
+  G.onActivateLeaf paste1 $ myPaste
+  delete1 <- get G.castToMenuItem "delete1"
+  G.onActivateLeaf delete1 $ myDelete
+
+  -- set up the Help-> About dialog
+  about1 <- get G.castToMenuItem "about1"
+  aboutdialog1 <- get G.castToAboutDialog "aboutdialog1"
+  G.onActivateLeaf about1 $ G.widgetShow aboutdialog1
+  G.onResponse aboutdialog1 $ const $ G.widgetHide aboutdialog1
+
+  -- fix size
+  --   G.windowSetResizable window False
+  G.widgetSetSizeRequest window windowWidth windowHeight
+
+  -- quit on File-> Quit menu selection
+  quit1 <- get G.castToMenuItem "quit1"
+  G.onActivateLeaf quit1 $ G.widgetDestroy window
+  G.onDestroy window G.mainQuit
+
+  -- set up the canvas
+  canvas <- get G.castToDrawingArea "drawingarea1"
+
+  return (window, canvas)
+
+
+
+-- Write image to file
+writePng :: IO ()
+writePng =
+  C.withImageSurface C.FormatARGB32 width height $ \ result -> do
+      C.renderWith result $ example width height []
+      C.surfaceWriteToPNG result "Draw.png"
+  where width  = windowWidth
+        height = windowHeight
+
+myFileSave :: G.FileChooserDialog -> G.ResponseId -> IO ()
+myFileSave = myFileOpen
+
+myCut :: IO ()
+myCut = putStrLn "Cut"
+
+myCopy :: IO ()
+myCopy = putStrLn "Copy"
+
+myPaste :: IO ()
+myPaste = putStrLn "Paste"
+
+myDelete :: IO ()
+myDelete = putStrLn "Delete"
